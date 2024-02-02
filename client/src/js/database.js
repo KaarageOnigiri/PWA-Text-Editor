fed19ea7d24a34de0f575b18b931635439aa7074
Villy Yeoh <yeohvilly@gmail.com>
2024-02-02 17:54:33 -0500

    added some base to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some contentDb = await openDB('text', 'readwrite'); and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT from the database');
+  const contentDb = await openDB('text', 1);
+  const tx = contentDb = await openDB('text', 'readwrite');
+  
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+// TODO: Add logic for a method that gets all the contentDb = await openDB('text', 'readwrite'); from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  const contentDb = await openDB('text', 1);
+  const tx = contentDb.transaction('text', 'readonly');
+  const store = tx.objectStore('text');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+// console.error('getDb not implemented');
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'Text Editor',
-        short_name: 'Text Editor',
+        short_name: 'JATE',
         description: 'Text Editor App',
         background_color: '#225ca3',
         theme_color: '#225ca3',
